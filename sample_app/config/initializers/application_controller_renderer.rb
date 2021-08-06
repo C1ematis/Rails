efd59bdf6cccea52c23ee85b07842be606043a38
EC2 Default User <ec2-user@ip-172-31-47-44.ec2.internal>
2021-08-06 14:09:58 +0000

    Finish Rails section8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a5b26028e86c55c8aaaa2399533d43c4c9b47770dbd4bef27589a771a6fc1918331aba0573224b47b37dab41bdd9e665e7c0841d54145b30c5596ddc4ea33bf6'
